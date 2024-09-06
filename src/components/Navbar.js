5ff42c1b2ed4ead39e8e73e867abf12b0b7b492e
Mauro Niro <maurogniro@gmail.com>
2024-09-06 15:53:27 -0300

    Created Main NavBar and CSS

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    return (
+        <nav className="nav">
+            <img className="nav-image" alt="world-image" src="https://cdn.discordapp.com/attachments/961634737187872771/1281643212938678382/image.png?ex=66dc76c1&is=66db2541&hm=a068083a8dafa161e4c4cc6aff25155735e250fc7497f127264469cfb2062811&"></img>
+            <p>Mi diario de viaje.</p>
+        </nav>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id: 1,
+        img: "https://scrimba.com/links/travel-journal-japan-image-url",
+        link: "https://www.google.com/maps/place/Monte+Fuji,+Kitayama,+Fujinomiya,+Prefectura+de+Shizuoka+418-0112,+Jap%C3%B3n/data=!4m2!3m1!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!5m1!1e4?sa=X&ved=1t:242&ictx=111",
+        country: "Japon",
+        place: "Monte Fuji",
+        date: "16 Octubre 2016 - 29 Octubre 2020",
+        description: "El monte Fuji es la montaña más alta de Japón, con una altura de 3.776 metros (12.380 pies). El monte Fuji es el sitio turístico más popular en Japón, tanto para los turistas japoneses como para los extranjeros."
+
+    },
+    {
+        id: 2,
+        img: "https://blogskystorage.s3.amazonaws.com/2021/07/skyairline_skyairline_image_182-1536x1024.jpeg",
+        link: "https://www.google.com/maps/place/Buenos+Aires,+Cdad.+Aut%C3%B3noma+de+Buenos+Aires/data=!4m2!3m1!1s0x95bcca3b4ef90cbd:0xa0b3812e88e88e87?sa=X&ved=1t:242&ictx=111",
+        country: "Argentina",
+        place: "Buenos Aires",
+        date: "15 Diciembre 2010 - 18 Octubre 2013",
+        description: "Buenos Aires es la capital de Argentina, se la considera la \"Paris\" de Latinoamerica"
+
+
+    },
+    {
+        id: 3,
+        img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/c9/6a/61/caption.jpg?w=1400&h=1400&s=1",
+        link: "https://www.google.com/maps/place/Washington+D.+C.,+Distrito+de+Columbia,+EE.+UU./data=!4m2!3m1!1s0x89b7c6de5af6e45b:0xc2524522d4885d2a?sa=X&ved=1t:242&ictx=111",
+        country: "Estados Unidos",
+        place: "Washington DC",
+        date: "18 Julio 2023 - 14 Octubre 24",
+        description: "La capital de Estados Unidos. Donde se encuentra la Casa Blanca"
+
+    }
+]
+export default data
