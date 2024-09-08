58fdb722412d7f799d59dc26c969a6523b5a1c05
Mauro Niro <maurogniro@gmail.com>
2024-09-08 04:23:46 -0300

    Better code

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,18 @@
+import pointer from '../images/pointer.png'
+
 export default function Main(props) {
     return (
-        <section className="main">
-            <img className="main-place-img" src={props.img}></img>
+        <section className='main'>
+            <img alt='' className='main-place-img' src={props.img}></img>
             <div>
-                <div className="main-div">
-                    <img src="https://cdn.discordapp.com/attachments/961634737187872771/1281657721799839816/image.png?ex=66dc8444&is=66db32c4&hm=4893b2b3e77e0f33d055c3540503d3044b3340e5d95b0bd311107151574dde32&"></img>
-                    <p className="main-country" >{props.country} </p>
-                    <a href={props.link} target="_blank" rel="noreferrer">Ver en Google Maps</a>
+                <div className='main-div'>
+                    <img alt='pointer' src={pointer}></img>
+                    <p>{props.country} </p>
+                    <a href={props.link} target='_blank' rel='noreferrer'>Ver en Google Maps</a>
                 </div>
-                <h1 className="main-place">{props.place}  </h1>
-                <h5 className="main-date">{props.date}</h5>
-                <p className="main-text">{props.description}</p>
+                <h1>{props.place}  </h1>
+                <h5>{props.date}</h5>
+                <p className='main-text'>{props.description}</p>
             </div>
         </section>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
+import worldimg from '../images/world-image.png'
+
 export default function NavBar() {
     return (
-        <nav className="nav">
-            <img className="nav-image" alt="world-image" src="https://cdn.discordapp.com/attachments/961634737187872771/1281643212938678382/image.png?ex=66dc76c1&is=66db2541&hm=a068083a8dafa161e4c4cc6aff25155735e250fc7497f127264469cfb2062811&"></img>
+        <nav className='nav'>
+            <img className='nav-image' alt='world-image' src={worldimg}></img>
             <p>Mi diario de viaje.</p>
         </nav>
     )
